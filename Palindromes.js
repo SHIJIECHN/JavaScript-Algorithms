00080029d41d6f2395b6f87ce876d7d1f9468747
shijiechn <shijiechn@aliyun.com>
2018-05-11 10:22:02 +0800

    添加回文方法二

--- Palindromes.js
+++ Palindromes.js
@@ -1,3 +1,4 @@
+// 方法一
 function palindrome(str) {
     var re = /[\W_]/g; // 匹配规则，\W是查找非单词字符，但是为了最后一个特例，所以把"_"下划线也加进去
     var str1 = str.toLowerCase(); // 将字符转换成小写
@@ -8,4 +9,37 @@ function palindrome(str) {
 
   palindrome("My age is 0, 0 si ega ym."); // 测试 true
   palindrome("not a palindrome"); // 测试 false.
-  
+
+
+// 方法二
+function palindrome(str){
+    // 把字符串中大写字母变成小写，并移除非单词字符
+    var cstr = str.toLowerCase().replace(/[^a-zA-Z0-9]+/g,'');
+    var count = 0;
+    // 
+    if(cstr===""){
+        alert("Nothing found!")
+        return false;
+    }
+
+    if((cstr.length) % 2 === 0){
+        ccount = (cstr.length) / 2;
+    }else{
+        if(cstr.length === 1){
+            alert("Entry is a palindrome.")
+            return turn;
+        }else{
+            ccount = (cstr.length-1) / 2;
+        }
+    }
+    for(var x = 0; x < ccount; x++){
+        if(cstr[x] != cstr.slice(-1-x)[0]){
+            alert("Entry is not a palindrome.");
+            return false;
+        }
+    }
+    alert("The entry is a palindrome.");
+    return true;
+}
+
+palindrome("0_0 (: /-\ :) 0-0"); // 测试 true
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # JavaScript-Algorithms
-总结JavaScript一些算法
+总结JavaScript一些算法   
 1、检查回文字符串
+
