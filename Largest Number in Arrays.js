17a145808dd9d5ff6a767878247e2bb08f5fb0b4
shijiechn <shijiechn@aliyun.com>
2018-05-11 16:26:51 +0800

    找出多个数组中的最大数

---
+++ Largest Number in Arrays.js
@@ -0,0 +1,34 @@
+// 方法一
+function largestOfFour(arr) {
+    // 新建一个数组用来存放最大值
+    var maxArr=[];
+    // 遍历数组
+    for(var i = 0; i < arr.length; i++){
+      // 初始化最大值是数组的第一项  
+      var maxNumber = arr[i][0];
+      // 遍历数组每一项
+      for(var j=1; j<arr[i].length; j++){
+        // 寻找最大值  
+        if(arr[i][j] > maxNumber){
+          maxNumber = arr[i][j];
+        }
+      }
+      // 把最大值保存到maxArr中
+      maxArr.push(maxNumber);
+    }
+    return maxArr;
+  }
+
+largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]); // [27,5,39,1001]
+
+
+// 方法二
+function largestOfFour(arr) {
+    return arr.map(function(subArr){ //sunArr是arr中的每一项
+        return subArr.reduce(function(pre, curr){ // pre curr是subArr中的前一项和当前项
+            return (curr > pre) ? curr : pre; // 返回最大值
+        }, 0); // 0是初始化值
+    });
+}
+
+largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]); // [27,5,39,1001]
--- Palindromes.js
+++ Palindromes.js
@@ -1,10 +1,15 @@
 // 方法一
 function palindrome(str) {
-    var re = /[\W_]/g; // 匹配规则，\W是查找非单词字符，但是为了最后一个特例，所以把"_"下划线也加进去
-    var str1 = str.toLowerCase(); // 将字符转换成小写
-    var oldStr = str1.replace(re, ''); //得到的原来字符串替换后的字符串
-    var newStr = oldStr.split('').reverse().join(''); // 把字符串进行翻转后，得到新的字符串
-    return oldStr == newStr; //判断翻转后的字符串和原字符串是否相等
+    // 匹配规则，\W是查找非单词字符，但是为了最后一个特例，所以把"_"下划线也加进去
+    var re = /[\W_]/g; 
+    // 将字符转换成小写
+    var str1 = str.toLowerCase(); 
+    //得到的原来字符串替换后的字符串
+    var oldStr = str1.replace(re, ''); 
+    // 把字符串进行翻转后，得到新的字符串
+    var newStr = oldStr.split('').reverse().join(''); 
+    //判断翻转后的字符串和原字符串是否相等
+    return oldStr == newStr; 
   }
 
   palindrome("My age is 0, 0 si ega ym."); // 测试 true
