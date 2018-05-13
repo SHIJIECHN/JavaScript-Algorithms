8b835eff8b903a5b0e1b439cc44257f918e3404c
shijiechn <shijiechn@aliyun.com>
2018-05-13 19:48:09 +0800

    摧毁数组

--- Seek and Destroy.js
+++ Seek and Destroy.js
@@ -32,4 +32,10 @@ function destroyer(arr) {
       }
       return arr;
   }
+  destroyer([1, 2, 3, 1, 2, 3], 2, 3); // [1, 1].
+  destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3); //[1, 5, 1].
+  destroyer([3, 5, 1, 2, 2], 2, 3, 5); // [1].
+  destroyer([2, 3, 2, 3], 2, 3); // [].
+  destroyer(["tree", "hamburger", 53], "tree", 53); // ["hamburger"]
+
   
