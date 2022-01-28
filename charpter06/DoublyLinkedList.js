0f52a1f3ca358ff598ba6a5d8f5f788e1bacd73e
shijie03 <shijie03@shengqugames.com>
2022-01-28 17:24:25 +0800

    双向链表

--- charpter06/DoublyLinkedList.js
+++ charpter06/DoublyLinkedList.js
@@ -2,32 +2,45 @@ import LinkedList from "./LinkedList.js";
 import { DoublyNode } from "./models/linked-list-models.js";
 import { defaultEquals } from "./utils/defaultEquals.js";
 
-export default class DoublyLinkedList extends LinkedList{
-  constructor(equalsFn = defaultEquals){
+export default class DoublyLinkedList extends LinkedList {
+  constructor(equalsFn = defaultEquals) {
     super(equalsFn);
     this.tail = undefined;
   }
 
-  insert(element, index){
-    if(index >= 0 && index < this.count){
+  push(element) {
+    const node = new DoublyNode(element);
+    if (this.head == null) {
+      this.head = node;
+      this.tail = node; // 新增
+    } else {
+      this.tail.next = node;
+      node.prev = this.tail;
+      this.tail = node;
+    }
+    this.count++
+  }
+
+  insert(element, index) {
+    if (index >= 0 && index <= this.count) {
       let node = new DoublyNode(element);
       let current = this.head;
-      if(index === 0){
-        if(this.head == null){
+      if (index === 0) {
+        if (this.head == null) {
           this.head = node;
           this.tail = node;
-        }else {
+        } else {
           node.next = current;
           current.prev = node;
           this.head = node;
         }
-      }else if(index === this.count){
+      } else if (index === this.count) {
         current = this.tail;
         current.next = node;
         node.prev = current;
         this.tail = node;
-      }else{
-        const previous = current.prev;
+      } else {
+        const previous = this.getElementAt(index - 1)
         node.next = current;
         previous.next = node;
         current.prev = node;
@@ -37,10 +50,36 @@ export default class DoublyLinkedList extends LinkedList{
       return true;
     }
     return false;
-    
   }
 
-  getTail(){
+  removeAt(index) {
+    if (index >= 0 && index < this.count) {
+      let current = this.head;
+      if (index === 0) { // 头部移除
+        this.head = current.next;
+        // 只有一个元素，更新tail 
+        if (this.count === 1) {
+          this.tail = undefined;
+        } else {
+          this.head.prev = undefined;
+        }
+      } else if (index === this.count - 1) { // 尾部移除
+        current = this.tail;
+        this.tail = current.prev;
+        this.tail.next = undefined;
+      } else { // 中间移除
+        let previous = this.getElementAt(index - 1);
+        current = previous.next;
+        previous.next = current.next;
+        current.next.prev = previous;
+      }
+      this.count--;
+      return current.element
+    }
+    return undefined
+  }
+
+  getTail() {
     return this.tail;
   }
 }
